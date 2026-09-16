e8c1a75852ff798b2d96c229687424c188086ef0
LAPTOP-45AH4I5V\nagat <nagatani@ay-sys.com>
2026-09-16 11:37:59 +0900

    toggleでdivをwrapするテストを追加

--- packages/extension-div/src/div.test.ts
+++ packages/extension-div/src/div.test.ts
@@ -9,12 +9,21 @@ describe("Div", () => {
     expect(editor.getJSON().content?.[0].type).toBe("div");
     destroyEditor(editor);
   });
+
   it("Divをunwrapできる", () => {
     const { editor } = createEditor([Div], "<div><p>テスト</p></div>");
     expect(editor.commands.unwrapDiv()).toBe(true);
     expect(editor.getJSON().content?.[0].type).toBe("paragraph");
     destroyEditor(editor);
   });
+
+  it("toggleDivでDivへwrapできる", () => {
+    const { editor } = createEditor([Div]);
+    expect(editor.commands.toggleDiv()).toBe(true);
+    expect(editor.getJSON().content?.[0].type).toBe("div");
+    destroyEditor(editor);
+  });
+
   it("Divをtoggleで解除できる", () => {
     const { editor } = createEditor([Div], "<div><p>テスト</p></div>");
     expect(editor.commands.toggleDiv()).toBe(true);
